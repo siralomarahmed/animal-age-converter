3a6a0119685061c615f7a18f84344c60eca1190d
Ahmed A. M. Alomar <ahmedamalomar@hotmail.com>
2019-09-17 09:07:39 +0300

    Adding app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// You can change your age here to test
+const myAge = 30;
+//________________
+
+
+const earlyDogYears = 2 * 10.5;
+const myAgeWithoutEarlyDogYears = myAge - 2;
+const laterDogYears = myAgeWithoutEarlyDogYears * 4;
+const myAgeInDogYears = earlyDogYears + laterDogYears;
+
+console.log(
+  `My age is ${myAge} so, my age in dog years is ${myAgeInDogYears}`
+);
